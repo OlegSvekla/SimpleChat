869cae0668427af6f256c66d2ff06a05e0a7952d
OlegSvekla <olegsvekla993@gmail.com>
2023-10-15 17:11:30 +0300

    add migrations, extebnsions for progaram

---
+++ src/SimpleChat.Api/Extensions/ServicesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace SimpleChat.Api.Extensions
+{
+    public static class ServicesConfiguration
+    {
+        public static void Configuration(
+            IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            //services.AddScoped(typeof(IEfRepository<>), typeof(EfRepository<>));
+
+            //services.AddScoped<IFilterService<PagedUserAndRoleResult>, UserService>();
+            //services.AddScoped<IUserService<User>, UserService>();
+
+            //services.AddScoped<IValidator<UserQueryParameters>, UserQueryParametersValidation>();
+            //services.AddScoped<IValidator<UserDto>, UserDtoValidation>();
+            //services.AddScoped<IValidator<RoleDto>, RoleDtoValidation>();
+
+            //services.AddAutoMapper(typeof(MapperEntityToDto));
+        }
+    }
+}
