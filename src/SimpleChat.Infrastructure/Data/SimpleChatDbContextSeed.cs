b1bcdad894fedac3985b299595be60885ce04794
OlegSvekla <olegsvekla993@gmail.com>
2023-10-15 16:51:40 +0300

    add seed class

--- src/SimpleChat.Infrastructure/Data/SimpleChatDbContextSeed.cs
+++ src/SimpleChat.Infrastructure/Data/SimpleChatDbContextSeed.cs
@@ -19,18 +19,18 @@ namespace SimpleChat.Infrastructure.Data
             {
                 logger.LogInformation("Data seeding started.");
 
-                // Заполнение начальных данных для моделей Chat, User, ChatUser и Message
-                if (!await dbContext.Chats.AnyAsync())
-                {
-                    await dbContext.Chats.AddRangeAsync(GetPreConfiguredChats());
-                    await dbContext.SaveChangesAsync();
-                }
+                //// Заполнение начальных данных для моделей Chat, User, ChatUser и Message
+                //if (!await dbContext.Chats.AnyAsync())
+                //{
+                //    await dbContext.Chats.AddRangeAsync(GetPreConfiguredChats());
+                //    await dbContext.SaveChangesAsync();
+                //}
 
-                if (!await dbContext.Users.AnyAsync())
-                {
-                    await dbContext.Users.AddRangeAsync(GetPreConfiguredUsers());
-                    await dbContext.SaveChangesAsync();
-                }
+                //if (!await dbContext.Users.AnyAsync())
+                //{
+                //    await dbContext.Users.AddRangeAsync(GetPreConfiguredUsers());
+                //    await dbContext.SaveChangesAsync();
+                //}
 
                 if (!await dbContext.ChatUsers.AnyAsync())
                 {
@@ -57,23 +57,27 @@ namespace SimpleChat.Infrastructure.Data
             }
         }
 
-        private static IEnumerable<Chat> GetPreConfiguredChats()
-        {
-            return new List<Chat>
-            {
-                new Chat { ChatName = "Chat 1" },
-                new Chat { ChatName = "Chat 2" },
-                new Chat { ChatName = "Chat 3" }
-            };
-        }
-
         private static IEnumerable<User> GetPreConfiguredUsers()
         {
             return new List<User>
             {
                 new User { Name = "User 1" },
                 new User { Name = "User 2" },
-                new User { Name = "User 3" }
+                new User { Name = "User 3" },
+                new User { Name = "User 4" },
+                new User { Name = "User 5" }
+            };
+        }
+
+        private static IEnumerable<Chat> GetPreConfiguredChats()
+        {
+            return new List<Chat>
+            {
+                new Chat { ChatName = "Chat 1", CreatorUserId = 1 },
+                new Chat { ChatName = "Chat 2", CreatorUserId = 2 },
+                new Chat { ChatName = "Chat 3", CreatorUserId = 3 },
+                new Chat { ChatName = "Chat 4", CreatorUserId = 4 },
+                new Chat { ChatName = "Chat 5", CreatorUserId = 5 },
             };
         }
 
@@ -87,8 +91,21 @@ namespace SimpleChat.Infrastructure.Data
             {
                 new ChatUser { UserId = users[0].Id, ChatId = chats[0].Id },
                 new ChatUser { UserId = users[1].Id, ChatId = chats[0].Id },
+                new ChatUser { UserId = users[2].Id, ChatId = chats[0].Id },
+
+                new ChatUser { UserId = users[0].Id, ChatId = chats[1].Id },
                 new ChatUser { UserId = users[1].Id, ChatId = chats[1].Id },
-                new ChatUser { UserId = users[2].Id, ChatId = chats[2].Id }
+                new ChatUser { UserId = users[4].Id, ChatId = chats[1].Id },
+
+                new ChatUser { UserId = users[2].Id, ChatId = chats[2].Id },
+                new ChatUser { UserId = users[3].Id, ChatId = chats[2].Id },
+                new ChatUser { UserId = users[4].Id, ChatId = chats[2].Id },
+
+                new ChatUser { UserId = users[0].Id, ChatId = chats[3].Id },
+                new ChatUser { UserId = users[4].Id, ChatId = chats[3].Id },
+
+                new ChatUser { UserId = users[1].Id, ChatId = chats[4].Id },
+                new ChatUser { UserId = users[3].Id, ChatId = chats[4].Id },
             };
 
             return chatUsers;
@@ -102,10 +119,23 @@ namespace SimpleChat.Infrastructure.Data
             // Здесь создаем сообщения для чатов
             var messages = new List<Message>
             {       
-                new Message { Text = "Hello, Chat 1!", UserId = users[0].Id, ChatId = chats[0].Id },
-                new Message { Text = "Hi there!", UserId = users[1].Id, ChatId = chats[0].Id },
-                new Message { Text = "Welcome to Chat 2", UserId = users[1].Id, ChatId = chats[1].Id },
-                new Message { Text = "Goodbye, Chat 3", UserId = users[2].Id, ChatId = chats[2].Id }
+                new Message { Content = "User 1 to Chat 1", UserId = users[0].Id, ChatId = chats[0].Id },
+                new Message { Content = "User 2 to Chat 1", UserId = users[1].Id, ChatId = chats[0].Id },
+                new Message { Content = "User 3 to Chat 1", UserId = users[2].Id, ChatId = chats[0].Id },
+
+                new Message { Content = "User 1 to Chat 2", UserId = users[0].Id, ChatId = chats[1].Id },
+                new Message { Content = "User 2 to Chat 2", UserId = users[1].Id, ChatId = chats[1].Id },
+                new Message { Content = "User 5 to Chat 2", UserId = users[4].Id, ChatId = chats[1].Id },
+
+                new Message { Content = "User 3 to Chat 3", UserId = users[2].Id, ChatId = chats[2].Id },
+                new Message { Content = "User 4 to Chat 3", UserId = users[3].Id, ChatId = chats[2].Id },
+                new Message { Content = "User 5 to Chat 3", UserId = users[4].Id, ChatId = chats[2].Id },
+
+                new Message { Content = "User 1 to Chat 4", UserId = users[0].Id, ChatId = chats[3].Id },
+                new Message { Content = "User 5 to Chat 4", UserId = users[4].Id, ChatId = chats[3].Id },
+
+                new Message { Content = "User 2 to Chat 5", UserId = users[1].Id, ChatId = chats[4].Id },
+                new Message { Content = "User 4 to Chat 5", UserId = users[3].Id, ChatId = chats[4].Id },
             };
 
             return messages;
